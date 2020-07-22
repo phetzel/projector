612a9c134599a3352f7517fce82e1b197a5f71fb
Phil <phetzel89@gmail.com>
2020-07-21 17:10:02 -0700

    Sign up/sign in/log in functionality

---
+++ frontend/components/session_form/login_form.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.login(user)
+            .then(this.props.closeModal)
+    }
+
+    renderErrors() {
+        return (
+            <ul className="errors">
+                {this.props.errors.map((error, i) => (
+                    <li key={`error-${i}`}>
+                        {error}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        return(
+            <div className="login-form-container">
+                <h1>Log In</h1>
+
+                <form className="login-form" onSubmit={this.handleSubmit}>
+                    <label>Email:
+                        <input type="text"
+                            className="login-input"
+                            value={this.state.email}
+                            onChange={this.update('email')}
+                        />
+                    </label>
+
+                    <br/>
+
+                    <label>Password:
+                        <input type="password"
+                            className="login-input"
+                            value={this.state.password}
+                            onChange={this.update('password')}
+                        />
+                    </label>
+
+                    <br/>
+
+                    <input id="login-submit" type="submit" value="Log In" />
+
+                    {this.renderErrors()}
+
+                </form>
+
+                <a onClick={this.props.signup}>Create an account?</a>
+
+            </div>
+        )
+    }
+}
+
+export default LoginForm;
---
+++ frontend/components/session_form/signup_form.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.signup(user).then(this.props.closeModal);
+    }
+
+    renderErrors() {
+        return (
+            <ul className="errors">
+                {this.props.errors.map((error, i) => (
+                    <li key={`error-${i}`}>
+                        {error}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        return (
+            <div className="signup-form-container">
+
+                <form className="signup-form" onSubmit={this.handleSubmit}>
+                    <h1>Sign Up</h1>
+
+                    <label>Email:
+                    <input type="text"
+                            className="login-input"
+                            value={this.state.email}
+                            onChange={this.update('email')}
+                        />
+                    </label>
+
+                    <br/>
+
+                    <label>Password:
+                    <input type="text"
+                            className="login-input"
+                            value={this.state.password}
+                            onChange={this.update('password')}
+                        />
+                    </label>
+
+                    <br/>
+
+                    <input id="signup-submit" type="submit" value="Sign Up" />
+
+                    {this.renderErrors()}
+
+                </form>
+
+                <a onClick={this.props.login}>Already have an account?</a>
+
+            </div>
+        )
+    }
+}
+
+export default SignupForm;
