120db78743a8dec9adee60225d94cfe10362c137
Phil <phetzel89@gmail.com>
2020-07-30 21:23:52 -0700

    Movie backend

---
+++ app/views/api/movies/_movie.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! movie, :id, :title, :run_time, :desc
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
     resources :shows, only: [:index, :show, :create]
+    resources :movies, only: [:index, :show, :create]
   end 
 end
