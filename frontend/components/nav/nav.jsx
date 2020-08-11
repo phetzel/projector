802bb9b9ae7f7e2582a9061344a29f142005b47b
Phil <phetzel89@gmail.com>
2020-08-10 21:29:41 -0700

    Logo Link for Nav Bar

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends React.Component {
     constructor(props) {
@@ -28,7 +29,12 @@ class Nav extends React.Component {
 
         return (
             <div className="nav">
-                <div className="nav-logo"></div>
+                <div className="nav-logo">
+                    <Link className="logo" to="/">
+                        <img src={window.logo} alt="Logo" />
+                    </Link>
+                </div>
+
                 <div className="nav-buttons">{currentUser ? loggedIn() : loggedOut()}</div>
             </div>
         )
