d1b007c36f924968a6a6057403b6f666d6586a95
Phil <phetzel89@gmail.com>
2020-09-03 05:02:32 -0700

    User Shows Links

--- frontend/components/user_show/user_show_list_item.jsx
+++ frontend/components/user_show/user_show_list_item.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import { revDate, displayTimeDay } from '../../util/data_util';
 
-const UserShowListItem = ({show}) => (
-    // <Link to={`/shows/${show.id}`}>
-        <li className="user-show-li">
-            <h6>{show.movie.title}</h6>
-            <div className="user-show-li-details">
-                <p>{show.address}</p>
+class UserShowListItem extends React.Component {
+    constructor(props) {
+        super(props);
 
-                <div className="user-show-li-when">
-                    <p>{displayTimeDay(show.time)}</p>
-                    <p>{revDate(show.date)}</p>
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const { show } = this.props;
+        this.props.history.push({
+            pathname: `shows/${show.id}`,
+            search: `lat=${show.lat}&lng=${show.lng}`,
+            lat: `${show.lat}`,
+            lng: `${show.lng}`
+        });
+}
+
+    render() {
+        const { show } = this.props;
+        return (
+            <li className="user-show-li" onClick={this.handleClick}>
+                <h6>{show.movie.title}</h6>
+                <div className="user-show-li-details">
+                    <p>{show.address}</p>
+
+                    <div className="user-show-li-when">
+                        <p>{displayTimeDay(show.time)}</p>
+                        <p>{revDate(show.date)}</p>
+                    </div>
                 </div>
-            </div>
-        </li>
-    // </Link >
-);
+            </li>
+        )
+    }
+}
 
-export default UserShowListItem;
+export default withRouter(UserShowListItem);
