40230ce64ec459d0225fea8f5c61e84f80ca8b92
Phil <phetzel89@gmail.com>
2020-09-27 08:58:48 -0700

    Fixing gitignore

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 50.megabytes)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
