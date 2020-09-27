5653cf53b538a988f00402c46bec68ad5d3d6d67
Phil <phetzel89@gmail.com>
2020-09-27 08:42:16 -0700

    Friend List Item Future Shows

--- frontend/components/user_show/user_friend_list_item.jsx
+++ frontend/components/user_show/user_friend_list_item.jsx
@@ -15,16 +15,34 @@ class UserFriendListItem extends React.Component {
         });
     }
 
+    futureShows() {
+        const { friend } = this.props;
+        const today = new Date();
+
+        let count = 0;
+
+        friend.shows.forEach(show => {
+            const showDate = new Date(show.date);
+
+            if (showDate > today) count++;
+        })
+
+        if (count != 0) {
+            return `Attending ${count} future shows`;
+        } else {
+            return '';
+        }
+    }
+
     render() {
         const { friend } = this.props;
+        const future = this.futureShows();
 
         return (
-            // <Link to={`/user/${this.props.friend.id}`}>
             <li className="user-show-li" onClick={this.handleClick}>
                     <h6>{friend.email}</h6>
+                    <p id="friend-shows">{future}</p>
             </li>
-            /* // </Link > */
-            
         )
     }
 }
